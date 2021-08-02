901da18b4242071e408a815836ba48a9f21d41e9
John Chilton <jmchilton@gmail.com>
2021-08-02 14:09:33 -0400

    Python 3.5 compat version of that.

--- edam_ontology/streams.py
+++ edam_ontology/streams.py
@@ -2,10 +2,12 @@
 
 Keep parsing and such to minimum to maintain maximum backward compatibility.
 """
-from importlib.resources import open_text
+from io import TextIOWrapper
 from typing import TextIO
+
+from pkg_resources import resource_stream
 
 
 def tabular_stream() -> TextIO:
     """Yield EDAM data in TSV format as a Python UTF-8 encoded text stream."""
-    return open_text("edam_ontology", 'EDAM.tsv')
+    return TextIOWrapper(resource_stream("edam_ontology", 'EDAM.tsv'), encoding='utf-8')
