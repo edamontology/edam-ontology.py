6529f13229e51bc2a9e1dd8f9a38e75ff63de646
Nicola Soranzo <nicola.soranzo@earlham.ac.uk>
2025-10-30 16:00:40 +0000

    Fix and cleanup release scripts

--- scripts/print_version_for_release.py
+++ scripts/print_version_for_release.py
@@ -1,18 +1,16 @@
-from __future__ import print_function
-
 import ast
 import re
 import sys
-from distutils.version import LooseVersion
+
+from packaging.version import Version
 
 source_dir = sys.argv[1]
 
-_version_re = re.compile(r'__version__\s+=\s+(.*)')
+_version_re = re.compile(r"__version__\s+=\s+(.*)")
 
-with open('%s/__init__.py' % source_dir, 'rb') as f:
-    version = str(ast.literal_eval(_version_re.search(
-        f.read().decode('utf-8')).group(1)))
+with open(f"{source_dir}/__init__.py", "rb") as f:
+    version = str(ast.literal_eval(_version_re.search(f.read().decode("utf-8")).group(1)))
 
 # Strip .devN
-version_tuple = LooseVersion(version).version[0:3]
+version_tuple = Version(version).release
 print(".".join(map(str, version_tuple)))
